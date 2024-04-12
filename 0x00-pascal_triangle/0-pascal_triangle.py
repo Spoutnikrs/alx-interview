87a64a21ba9c18b5e0e970f897588f4e002945f6
rayesyounes <rayesyounes88@gmail.com>
2024-04-12 19:52:38 +0000

    fix format ?

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,8 +11,8 @@ def pascal_triangle(n):
         row = [1]
         for j in range(1, row_number):
             element = (
-                    pascal_triangle[row_number - 1][j - 1]
-                    + pascal_triangle[row_number - 1][j]
+                pascal_triangle[row_number - 1][j - 1]
+                + pascal_triangle[row_number - 1][j]
             )
             row.append(element)
         row.append(1)
