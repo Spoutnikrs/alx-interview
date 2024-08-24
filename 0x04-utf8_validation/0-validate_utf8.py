d254051b0d7fab286c8500af109c4705c1896c52
rayesyounes <rayesyounes88@gmail.com>
2024-08-24 23:29:32 +0100

    WTF8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python3
-"""define validUTF8 function"""
+"""Define function validUTF8"""
 
 
 def validUTF8(data):
-    pass
+    def is_continuation(byte):
+        return (byte & 0b11000000) == 0b10000000
+
+    i = 0
+    while i < len(data):
+        byte = data[i]
+        if byte >> 3 == 0b11110:  # 4-byte character
+            length = 4
+        elif byte >> 4 == 0b1110:  # 3-byte character
+            length = 3
+        elif byte >> 5 == 0b110:  # 2-byte character
+            length = 2
+        elif byte >> 7 == 0:  # 1-byte character
+            length = 1
+        else:
+            return False  # Invalid leading byte
+
+        if i + length > len(data) or any(not is_continuation(data[i + j]) for j in range(1, length)):
+            return False
+        i += length
+
+    return True
